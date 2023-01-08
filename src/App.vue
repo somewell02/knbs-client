06b2f11285487e656b5fd9b9e337f3758d7b62cc
Samvel Grigoryan <samvel121202@mail.ru>
2023-01-08 17:57:09 +0300

    Init commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-app>
+    <v-main>
+      <router-view/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import {mapActions} from "vuex";
+
+export default {
+  name: 'App',
+
+  mounted() {
+    this.getUserAuth();
+  },
+
+  methods: {
+    ...mapActions({
+      getUserAuth: 'user/getUserAuth'
+    })
+  }
+}
+</script>
+
+<style src="./assets/styles.css"></style>
